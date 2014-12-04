458695a866f63731b52cdc77c81c2743146ae6e6
Whyte <whyted@longlhrkc000466.emea.corp.yr.com>
2014-12-04 16:33:18 +0000

    responsive-site-added

---
+++ app.js
@@ -0,0 +1,13 @@
+$("#menu").click(function(){
+  $("#header-ul").slideToggle(1000);
+});
+
+$(window).on('resize', function(){
+    
+    if($(this).width() > 640) {
+      
+      $("#header-ul").removeAttr('style');
+    }
+ }); 
+  
+  
